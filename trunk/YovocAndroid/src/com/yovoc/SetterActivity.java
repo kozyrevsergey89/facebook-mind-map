be246c0f3cef8619bfe3e42a58775e1aca67c792
david.ftzi <david.ftzi@1f42f92b-368d-acfd-d7f5-172864f34d95>
2012-04-22 01:42:33 +0000

    camera updates

--- trunk/YovocAndroid/src/com/yovoc/SetterActivity.java
+++ trunk/YovocAndroid/src/com/yovoc/SetterActivity.java
@@ -1,6 +1,16 @@
 package com.yovoc;
 
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
 public class SetterActivity extends BasicActivity {
 
+	@Override
+	protected void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Uri uri = (Uri)getIntent().getExtras().get("path");
+		Log.i("Yovoc", uri.getPath());
+	}
 	
 }
