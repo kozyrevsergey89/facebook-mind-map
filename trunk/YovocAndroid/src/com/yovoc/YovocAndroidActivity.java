260962ddd0280348d78ef4dc348ab19154b35a90
david.ftzi <david.ftzi@1f42f92b-368d-acfd-d7f5-172864f34d95>
2012-04-22 00:29:44 +0000

    new functionality

--- trunk/YovocAndroid/src/com/yovoc/YovocAndroidActivity.java
+++ trunk/YovocAndroid/src/com/yovoc/YovocAndroidActivity.java
@@ -3,50 +3,25 @@ package com.yovoc;
 import com.yovoc.service.YovocService;
 import com.yovoc.service.YovocService.YovocBinder;
 
-import media.YovocMediaPlayer;
-import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.widget.TextView;
 
-public class YovocAndroidActivity extends Activity {
-    private String sharedText;
-	private String sharedContentType;
-	private YovocMediaPlayer ymp;
+public class YovocAndroidActivity extends BasicActivity {
 	private YovocService yovocService;
 	private boolean bound;
 	private YovocServiceConnection serviceConnection; 
-	//private YovocProvider dataProvider;
-
-	/** Called when the activity is first created. */
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        /*
-        dataProvider = new YovocProvider(this);
-        dataProvider.open();
-        dataProvider.createTermin("hackathon", "coders competition");
-        dataProvider.createTermin("coder", "machine for translating coffee into code");
-        */
-        
-        
         TextView textview=(TextView)findViewById(R.id.testView); 
-       /*
-        String str = "";
-        for(TermModel termin : dataProvider.getAllTermins()) {
-        	str += ", " + termin.getTermin() + " " + termin.getDescription();
-        }
-        textview.setText(str);
-        */
-        
         serviceConnection = new YovocServiceConnection();
         
         Intent intent = getIntent();
@@ -55,10 +30,6 @@ public class YovocAndroidActivity extends Activity {
             if(intent.getType().equals("text/plain")){
             	String textToView = intent.getStringExtra(Intent.EXTRA_TEXT).toString();
                 textview.setText(textToView);
-                
-                
-                
-                
             }
             else if (intent.getType().equals("image/*")){
             	//to do 
@@ -82,7 +53,6 @@ public class YovocAndroidActivity extends Activity {
 	}
 	
 	//disbinding from service
-	
 	@Override
 	protected void onStop() {
 		super.onStop();
@@ -102,15 +72,9 @@ public class YovocAndroidActivity extends Activity {
 			yovocService = yovocBinder.getService();
 			bound = true;
 		}
-
+		
 		public void onServiceDisconnected(final ComponentName name) { bound = false; }
 	}
-    
-    @Override
-    public void onDestroy() {
-    	ymp.yovocMPStop();
-        super.onDestroy();
-}
     
     
 }
